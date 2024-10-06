d6c7f12f3d3c9a499f6eaf0e2ff12036eadf4a5f
Vishnu T <vishnuthenghanal@gmail.com>
2024-10-06 11:25:36 +0530

    initial-commit

---
+++ src/test/java/com/codan/serviceregistryapp/ServiceRegistryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codan.serviceregistryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
